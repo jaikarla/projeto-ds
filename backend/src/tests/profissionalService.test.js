adc33644f735b23c9ab8819b15d8fbac0bfe8753
Ana Clara <anaclarasouzaif@gmail.com>
2026-06-04 10:42:20 -0300

    fix: correção de testes de profissionais

--- backend/src/tests/profissionalService.test.js
+++ backend/src/tests/profissionalService.test.js
@@ -1,20 +1,9 @@
-//testes do services de profissional
-
-//IMPORTS -----------
 import { describe, test, expect, beforeEach, vi } from "vitest";
-//describe - utilizada para agrupar testes relacionados;
-//test - define um caso de teste específico;
-//expect - é usada para fazer asserções, ou seja, verificar se os resultados dos testes estão de acordo com o esperado.
-//beforeEach - é uma função que é executada antes de cada teste, permitindo configurar um ambiente de teste consistente.
 
-//importando a função CRIAR de profissional para testar
 import { criarProfissional } from "../services/profissionalService";
-import { listarProfissionais } from "../services/profissionalService"; //função listar
-import { buscarProfissionalPorId } from "../services/profissionalService"; //função buscar por id
 import { atualizarProfissional } from "../services/profissionalService"; //função atualizar
-import { deletarProfissional } from "../services/profissionalService"; //função deletar
 
-import Profissional from '../models/profissional.js'; //importando o modelo de profissional para mockar os dados e evitar interações reais com o banco de dados durante os testes
+import Profissional from '../models/profissional.js'; 
 
 //MOCKS -----------
 vi.mock('../models/profissional.js', () => ({
@@ -22,17 +11,18 @@ vi.mock('../models/profissional.js', () => ({
     buscar_profissional_cpf: vi.fn(),
     buscar_profissional_cro: vi.fn(),
     buscar_profissional_cns: vi.fn(),
-    criar_profissional: vi.fn()
+    criar_profissional: vi.fn(),
+    atualizar_dados_profissional: vi.fn()
   }
 }));
 
-//TESTES CRIAR PROFISSIONAL -----------
+
 describe("Testes para criar profissional", () => {
 
-    //Teste 1
-    test("Deve criar um profissional válido",
-    async () => {
-        //mockando as funções de busca
+    beforeEach(() => { vi.clearAllMocks(); });
+
+    test("Deve criar um profissional válido", async () => {
+
         Profissional.buscar_profissional_cpf.mockResolvedValue(null);
         Profissional.buscar_profissional_cro.mockResolvedValue(null);
         Profissional.buscar_profissional_cns.mockResolvedValue(null);
@@ -41,164 +31,360 @@ describe("Testes para criar profissional", () => {
             nome: "João Silva"
         });
 
-    const profissional =
-      await criarProfissional({
+    const profissional = await criarProfissional({
 
-        cpf: "12345678901",
-        nome: "João Silva",
-        cro: "PE-12345",
-        cro_uf: "PE",
-        cbo: "Dentista",
-        matricula: null,
-        tipo: "profissional",
-        cns: "123456789012345"
+        nome: 'João Silva',
+        cpf: '12345678901',
+        cbo: 'Dentista',
+        tipo: 'Profissional',
+        matricula: '',
+        cns: '123456789012345',
+        cro: 'PE-12345',
+        cro_uf: 'PE'
 
       });
     
-    //espera-se que o profissional criado tenha o nome seja "João Silva"
     expect(profissional.nome).toBe("João Silva");
 
   }
 );
 
-    //Teste 2
-    test.skip("Deve exibir erro para cpf inválido.", () => {
-        expect(() => {
+    //teste 2
+    test("Deve exibir erro qundo campo obrigatório estiver vazio", async () => {
 
-            criarProfissional({
-                nomeCompleto: "Raiana Donato",
-                cpf: "12345", //cpf inválido pela quantidade de dígitos
-                cro: "CRO-54321",
-                ufConselho: "PE",
-                cns: "543210987654321",
-                especialidade: "Ortodontista",
-            })
-        }).toThrow("CPF inválido. Deve conter exatamente 11 dígitos numéricos.");
+        await expect(criarProfissional({
+            nome: '',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'Profissional',
+            matricula: '',
+            cns: '123456789012345',
+            cro: 'PE-12345',
+            cro_uf: 'PE'
+        
+        })).rejects.toThrow("Todos os campos obrigatórios devem ser preenchidos.");
+    
     });
 
     //Teste 3
-    test.skip("Deve exibir erro para cro inválido.", () => {
-        expect(() => {
+    test("Deve exibir erro para CPF inválido.", async () => {
 
-            criarProfissional({
-                nomeCompleto: "Alana Silva",
-                cpf: "98765432109",
-                cro: "CR-12345", //cro inválido pelo formato
-                ufConselho: "PE",
-                cns: "987654321012345",
-                especialidade: "Endodontista",
-            })
-        }).toThrow("CRO inválido. Verifique o formato correto (ex: CRO-12345).");
+        await expect(criarProfissional({
+            nome: 'Raiana Lima',
+            cpf: '1234567890🪲',
+            cbo: 'Dentista',
+            tipo: 'Estudante',
+            matricula: '1234567890',
+            cns: '123456789012345',
+            cro: '',
+            cro_uf: ''
+        
+        })).rejects.toThrow("CPF deve conter 11 dígitos numéricos.");
     });
 
     //Teste 4
-    test.skip("Deve exibir erro para cns inválido.", () => {
-        expect(() => {
-
-            criarProfissional({
-                nomeCompleto: "Bruna Oliveira",
-                cpf: "56789012345",
-                cro: "CRO-67890",
-                ufConselho: "PE",
-                cns: "12345", //cns inválido pela quantidade de dígitos
-                especialidade: "Periodontista",
-            })
-        }).toThrow("CNS inválido. Deve conter exatamente 15 dígitos numéricos.")
-    })
-
-    //Teste 5
-    test("Deve exibir erro por duplicidade - cpf já cadastrado.",
-    async () => {
-
-    //mockando a função de busca por cpf para simular que já existe um profissional com o cpf fornecido
-    Profissional.buscar_profissional_cpf.mockResolvedValue({
-        id: 1
-      });
-
+    test("Deve exibir erro para CROinválido.", async () => {
+        
         await expect(criarProfissional({
-                cpf: "12345678901",
-                nome: "João Silva",
-                cro: "PE-12345",
-                cro_uf: "PE",
-                cbo: "Dentista",
-                cargo: "Dentista",
-                tipo: "profissional",
-                cns: "123456789012345"
+            nome: 'João Silva',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'Profissional',
+            matricula: '1234567890',
+            cns: '123456789012345',
+            cro: 'PEC-12345',
+            cro_uf: 'PE'
 
-            })
-
-        ).rejects.toThrow("CPF já cadastrado.");
-
+        })).rejects.toThrow("CRO inválido. Use XX-12345.");
     });
 
+    //Teste 5
+    test("Deve exibir erro para CNS inválido.",  async () => {
+        
+        await expect(criarProfissional({
+            nome: 'João Silva',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'Profissional',
+            matricula: '1234567890',
+            cns: '123456789012🍎',
+            cro: 'PEC-12345',
+            cro_uf: 'PE'
+
+            })).rejects.toThrow("CNS deve conter 15 dígitos numéricos.")
+    })
+
     //Teste 6
-    test("Deve exibir erro por duplicidade - cro já cadastrado.", 
-    async () => {
+    test("Deve exibir erro para tipo inválido.", async () => {
 
-    //mockando a função de busca por cpf para simular que já existe um profissional com o cpf fornecido
-    Profissional.buscar_profissional_cpf.mockResolvedValue({
-        id: 1
-      });
+        await expect(criarProfissional({
+            nome: 'João Silva',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'Admin',
+            matricula: '1234567890',
+            cns: '123456789012345',
+            cro: 'PEC-12345',
+            cro_uf: 'PE'
 
-        await criarProfissional({
-            nomeCompleto: "Carla Souza",
-            cpf: "11122233324",
-            cro: "CRO-56038", //cro específico para o teste de duplicidade
-            ufConselho: "PE",
-            cns: "543210987654321",
-            especialidade: "Cirurgiã Dentista",
-        });
-
-        expect(() => {
-            //criamos outro profissional com o mesmo cro
-            criarProfissional({
-                nomeCompleto: "Mariana Lima",
-                cpf: "22233344455",
-                cro: "CRO-56038",
-                ufConselho: "PE",
-                cns: "543210987654331",
-                especialidade: "Cirurgiã Dentista",
-            })
-        }).toThrow("CRO já cadastrado.");
+        })).rejects.toThrow("Tipo deve ser profissional ou estudante.");
     });
 
     //Teste 7
-    test.skip("Deve exibir erro por duplicidade - cns já cadastrado.", () => {
-        //primeiro, criamos um profissional com um cns específico
-        criarProfissional({
-            nomeCompleto: "Carla Souza",
-            cpf: "11123233344",
-            cro: "CRO-54331",
-            ufConselho: "PE",
-            cns: "345679240163972", //cns específico para o teste de duplicidade
-            especialidade: "Cirurgiã Dentista",
+    test('Deve exibir erro quando estudante não informar matrícula.', async () => {
+
+    await expect(criarProfissional({
+        nome: 'Maria Souza',
+        cpf: '12345678901',
+        cbo: 'Estudante',
+        tipo: 'estudante',
+        cns: '123456789012345'
+
+      })).rejects.toThrow('Matrícula é obrigatória para estudante.');
+    });
+
+    //Teste 8
+    test('Deve exibir erro quando profissional não informar CRO.', async () => {
+
+        await expect(criarProfissional({
+            nome: 'João Silva',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'profissional',
+            cns: '123456789012345',
+            cro_uf: 'PE'
+
+        })).rejects.toThrow('Todos os campos obrigatórios devem ser preenchidos.');
+    });
+
+    //Teste 9
+    test('Deve permitir estudante sem CRO e CRO-UF', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue(null);
+        Profissional.buscar_profissional_cns.mockResolvedValue(null);
+
+        Profissional.criar_profissional.mockResolvedValue({
+            id: 2,
+            nome: 'Maria Souza'
         });
 
-        expect(() => {
-            //criamos outro profissional com o mesmo cns
-            criarProfissional({
-                nomeCompleto: "Mariana Lima",
-                cpf: "99988877766",
-                cro: "CRO-67890",
-                ufConselho: "PE",
-                cns: "345679240163972",
-                especialidade: "Cirurgiã Dentista",
-            })
-        }).toThrow("CNS já cadastrado.");
+        const resultado = await criarProfissional({
+        nome: 'Maria Souza',
+        cpf: '12345678901',
+        cbo: 'Estudante',
+        tipo: 'estudante',
+        cns: '123456789012345',
+        matricula: '20250001'
+        });
+
+        expect(resultado.id).toBe(2);
     });
+
+    //Teste 10
+    test('Deve exibir erro para CPF duplicado', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue({
+            id: 1
+        });
+
+        await expect(criarProfissional({
+            nome: 'João Silva',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'profissional',
+            cns: '123456789012345',
+            cro: 'PE-12345',
+            cro_uf: 'PE'
+
+        })).rejects.toThrow('CPF já cadastrado.');
+    });
+
+    //Teste 11
+    test('Deve exibir erro para CNS duplicado', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue(null);
+        Profissional.buscar_profissional_cns.mockResolvedValue({
+            id: 1
+        });
+
+        await expect(criarProfissional({
+            nome: 'João Silva',
+            cpf: '12345678902',
+            cbo: 'Dentista',
+            tipo: 'profissional',
+            cns: '123456789012345',
+            cro: 'PE-12345',
+            cro_uf: 'PE'
+
+        })).rejects.toThrow('CNS já cadastrado.');
+    });
+
+    //Teste 12
+    test('Deve exibir erro para CRO duplicado', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue(null);
+        Profissional.buscar_profissional_cns.mockResolvedValue(null);
+
+        Profissional.buscar_profissional_cro.mockResolvedValue({
+            id: 1
+        });
+
+        await expect(criarProfissional({
+            nome: 'João Silva',
+            cpf: '12345678903',
+            cbo: 'Dentista',
+            tipo: 'profissional',
+            cns: '123456789012345',
+            cro: 'PE-12345',
+            cro_uf: 'PE'
+            
+        })).rejects.toThrow('CRO já cadastrado.');
+    });
+
 });
 
-//TESTES LISTAR PROFISSIONAL -----------
-describe("Testes para listar profissionais", () => {
 
-    test.skip("Deve listar todos os profissionais cadastrados", () => {
-        const profissionais = listarProfissionais();
-        expect(Array.isArray(profissionais)).toBe(true); //verifica se o resultado é um array
+describe("Testes para atualizar profissional", () => {
+
+    //Teste 1
+    test('Deve atualizar um profissional válido.', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue({
+            id: 1
+        });
+
+        Profissional.buscar_profissional_cns.mockResolvedValue({
+            id: 1
+        });
+
+        Profissional.buscar_profissional_cro.mockResolvedValue({
+            id: 1
+        });
+
+        Profissional.atualizar_dados_profissional.mockResolvedValue({
+            id: 1,
+            nome: 'João Atualizado'
+        });
+
+        const resultado = await atualizarProfissional(
+            1,
+            {
+            nome: 'João Atualizado',
+            cpf: '12345678901',
+            cbo: 'Dentista',
+            tipo: 'profissional',
+            cns: '123456789012345',
+            cro: 'PE-12345',
+            cro_uf: 'PE'
+            }
+        );
+
+        expect(resultado.nome).toBe('João Atualizado');
     });
+
+    //Teste 2
+    test('Deve exibir erro para CPF já cadastrado por outro profissional.', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue({
+            id: 2
+        });
+
+        await expect(atualizarProfissional(
+            1,
+            {
+                nome: 'João Silva',
+                cpf: '12345678901',
+                cbo: 'Dentista',
+                tipo: 'profissional',
+                cns: '123456789012345',
+                cro: 'PE-12345',
+                cro_uf: 'PE'
+
+            })).rejects.toThrow('CPF já cadastrado.');
+    });
+
+    //Teste 3
+    test('Deve exibir erro para CNS já cadastrado por outro profissional.', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue({
+            id: 1
+        });
+
+        Profissional.buscar_profissional_cns.mockResolvedValue({
+            id: 2
+        });
+
+        await expect(atualizarProfissional(
+            1,
+            {
+                nome: 'João Silva',
+                cpf: '12345678901',
+                cbo: 'Dentista',
+                tipo: 'profissional',
+                cns: '123456789012345',
+                cro: 'PE-12345',
+                cro_uf: 'PE'
+
+            })).rejects.toThrow('CNS já cadastrado.');
+    });
+
+    //Teste 4
+    test('Deve exibir erro para CRO já cadastrado por outro profissional.', async () => {
+
+        Profissional.buscar_profissional_cpf.mockResolvedValue({
+            id: 1
+        });
+
+        Profissional.buscar_profissional_cns.mockResolvedValue({
+            id: 1
+        });
+
+        Profissional.buscar_profissional_cro.mockResolvedValue({
+            id: 2
+        });
+
+        await expect(atualizarProfissional(
+            1,
+            {
+                nome: 'João Silva',
+                cpf: '12345678901',
+                cbo: 'Dentista',
+                tipo: 'profissional',
+                cns: '123456789012345',
+                cro: 'PE-12345',
+                cro_uf: 'PE'
+            })).rejects.toThrow('CRO já cadastrado.');
+    });
+
+    //Teste 5
+    test('Deve exibir erro quando estudante não informar matrícula.', async () => {
+
+        await expect(atualizarProfissional(
+            1,
+            {
+                nome: 'Maria',
+                cpf: '12345678901',
+                cbo: 'Estudante',
+                tipo: 'estudante',
+                cns: '123456789012345'
+
+            })).rejects.toThrow('Matrícula é obrigatória para estudante.');
+    });
+
+    //Teste 6
+    test('Deve exibir erro para tipo inválido', async () => {
+
+        await expect(
+            atualizarProfissional(
+            1,
+            {
+                nome: 'João',
+                cpf: '12345678901',
+                cbo: 'Dentista',
+                tipo: 'admin',
+                cns: '123456789012345'
+            
+            })).rejects.toThrow('Tipo deve ser profissional ou estudante.');
+    });
+
 });
-
-//TESTE BUSCAR POR ID -----------
-
-//TESTE ATUALIZAR PROFISSIONAL -----------
-
-//TESTE DELETAR PROFISSIONAL -----------
