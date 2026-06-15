a953bbb6a167ede2600f70fd0d6bc109c35a9020
Ana Clara <anaclarasouzaif@gmail.com>
2026-06-15 15:24:58 -0300

    fix(routes): retirando comentarios desnecessários

--- backend/src/routes/estudantes.routes.js
+++ backend/src/routes/estudantes.routes.js
@@ -17,30 +17,3 @@ router.put("/:id", updateEstudante);
 router.delete("/:id", deleteEstudante);
 
 export default router;
-
-//-------------- ROTAS PARA TESTES --------------
-//essas rotas são apenas para fins de teste e devem ser removidas ou substituídas por implementações reais posteriormente
-//mantenhas como comentário. Use-as apenas p/ verificar as rotas quando necessário. lembre-se de comentar as de cima para realizar os teste com as de baixo, e vice-versa. 
-
-/*
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Listando estudantes" });
-});
-
-router.get("/:id", (req, res) => {
-  res.status(200).json({ message: `Buscando estudante ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(201).json({ message: "Estudante criado com sucesso" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Estudante ${req.params.id} atualizado` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Estudante ${req.params.id} removido` });
-});
-
-export default router;*/
--- backend/src/routes/profissionais.routes.js
+++ backend/src/routes/profissionais.routes.js
@@ -18,29 +18,3 @@ router.delete("/:id", deleteProfissional);
 
 export default router;
 
-//-------------- ROTAS PARA TESTES --------------
-//essas rotas são apenas para fins de teste e devem ser removidas ou substituídas por implementações reais posteriormente
-//mantenhas como comentário. Use-as apenas p/ verificar as rotas quando necessário. lembre-se de comentar as de cima para realizar os teste com as de baixo, e vice-versa.
-
-/*
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Listando profissionais" });
-});
-
-router.get("/:id", (req, res) => {
-  res.status(200).json({ message: `Buscando profissional ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(201).json({ message: "Profissional criado com sucesso" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Profissional ${req.params.id} atualizado` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Profissional ${req.params.id} removido` });
-});
-
-export default router;*/
